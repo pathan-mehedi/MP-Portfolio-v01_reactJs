63d4e9e40990eb06ce3768c3eba96ea12aaf90ab
MehediPathan <eity.mehedipathan@gmail.com>
2023-05-05 21:24:15 +0600

    some icon and social media linked are solved 02

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import React from "react";
 // import { NavLink } from 'react-router-dom'
 import styled from "styled-components";
-import { Facebook, Github, InstaGram, LinkedIn, Twitter, YouTube } from "../components/AllSvgs";
+import { Facebook, Github, InstaGram, LinkedIn, Twitter } from "../components/AllSvgs";
 import { DarkTheme } from "../components/Themes";
 
 const Icons = styled.div`
