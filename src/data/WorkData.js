ad962f3bba54570a4be7896f3fe5f11665fd0047
Mehedi Pathan <eity.mehedipathan@gmail.com>
2023-05-27 15:09:58 +0600

    readme and artical section edited v_0.5

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -8,54 +8,44 @@ export const Work = [
         tags:["react","gsap","styledComponents"],
         
         demo:"https://mts-agency-landing.netlify.app/",
-        github:"https://github.com/codebucks27/Agency-website"
+        github:"https://github.com/"
     },
     {
         id:2,
-        name:"Job listing website",
-        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
-        tags:["react","sass"],
-        
-        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/react-job-listing-website"
-        
-    },
-    {
-        id:3,
         name:"Dev.to Clone",
         description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
         tags:["react","api","sass"],
         
-        demo:"http://devto-clone-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/devto-clone"
+        demo:"https://mts-devto-clone.netlify.app/",
+        github:"https://github.com/"
     },
     {
-        id:4,
+        id:3,
         name:"Sidebar Menu",
         description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
         tags:["react","styledComponents","react-router"],
         
         demo:"https://react-sidebar.vercel.app/",
-        github:"https://github.com/codebucks27/react-sidebar"
+        github:"https://github.com/"
        
     },{
-        id:5,
+        id:4,
         name:"Todo App(React + Redux)",
         description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
         tags:["react","redux","framer-motion"],
         
         demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        github:"https://github.com/"
         
     },
     {
-        id:6,
+        id:5,
         name:"Responsive Navigation Menu",
         description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
         tags:["react","react-router","css"],
         
         demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
+        github:"https://github.com/"
         
     },
     
